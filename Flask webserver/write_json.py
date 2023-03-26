28559056c5ebaa3eb151d59457bf9dd66a8d02e1
Aaron <aarontenzing@gmail.com>
2023-03-26 15:05:33 +0200

    Update write_json.py

--- Flask webserver/write_json.py
+++ Flask webserver/write_json.py
@@ -1,13 +1,5 @@
 import json
 
-<<<<<<< HEAD
-def write(data):
-    with open('data_list.json', 'r+') as f:
-        file_data = json.load(f)    
-        print(file_data)    
-        file_data["log"].append(json.load(data))
-        json.dump(file_data, f) # converts python dict to json
-=======
 def write(data, filename):
     
     with open(filename, 'r') as f:
@@ -23,4 +15,3 @@ def write(data, filename):
 
     with open(filename, 'w') as f:
         json.dump(file_data, f, indent=4) # dict to array (json)
->>>>>>> f3fdd4032ebd9f3e692863af78a5bb44b6c43c0d
