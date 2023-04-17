d41c6c101f4cc327433e8fd327119ee5866594ee
tenzing <aarontenzing@gmail.com>
2023-04-17 09:39:44 +0200

    some changes

--- Flask webserver/queue_list.py
+++ Flask webserver/queue_list.py
@@ -7,12 +7,15 @@ def add_queue(cid, user_list, time_list):
     if(cid not in user_list):
         user_list.append(cid)
         time_list.append(strftime("%H:%M:%S"))
-        write(str({"cid":cid, "time": strftime("%H:%M:%S")}))
+        playsound("Flask webserver\join.mp3")
+        write({'cid':cid, 'time': strftime("%H:%M:%S"),'entry':'joined'}, 'Flask webserver\log.json')
         return user_list, time_list
     
     else:
         for i in range(len(user_list)):
             if(cid == user_list[i]):
                 del user_list[i], time_list[i]
+                playsound("Flask webserver\leave.mp3")
+                write({'cid':cid, 'time': strftime("%H:%M:%S"),'entry':'left'}, 'Flask webserver\log.json')
                 return user_list, time_list
 
